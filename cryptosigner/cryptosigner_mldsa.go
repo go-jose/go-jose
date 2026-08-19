7f9a62fafb8a8a6988e637190a4eca0c89b790bf
James Elliott <james-d-elliott@users.noreply.github.com>
2026-08-20 09:13:39 +1000

    refactor: copyright

--- cryptosigner/cryptosigner_mldsa.go
+++ cryptosigner/cryptosigner_mldsa.go
@@ -1,7 +1,7 @@
 //go:build go1.27
 
 /*-
- * Copyright 2018 Square Inc.
+ * Copyright 2026 The Go JOSE Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
